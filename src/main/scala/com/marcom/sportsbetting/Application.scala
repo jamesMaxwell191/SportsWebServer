8e9dd91d1a84bac8ef58b98ab82681ff7e435a5e
douglas.maxwell-salmon@outlook.com <douglas.maxwell-salmon@outlook.com>
2017-01-02 16:24:51 +0000

    added cluster pub sub support

--- src/main/scala/com/marcom/sportsbetting/Application.scala
+++ src/main/scala/com/marcom/sportsbetting/Application.scala
@@ -1,19 +1,21 @@
 package com.marcom.sportsbetting
 
 import akka.actor.ActorSystem
+import akka.cluster.Cluster
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
+
 import scala.io.StdIn
 
 
 object WebServer {
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
+    implicit val system = ActorSystem("ClusterSystem")
     implicit val materializer = ActorMaterializer()
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
@@ -27,10 +29,20 @@ object WebServer {
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+    val cluster = Cluster(system)
+    cluster.registerOnMemberUp {
+
+    }
+    cluster.registerOnMemberRemoved{
+      system.registerOnTermination {
+        System.exit(1)
+      }
+
+      bindingFuture
+        .flatMap(_.unbind()) // trigger unbinding from the port
+        .onComplete(_ => system.terminate()) // and shutdown when done
+
+    }
+
   }
 }
